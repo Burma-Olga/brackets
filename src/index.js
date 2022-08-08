04e6d85285aacdb9f5e6cc7d5abfbdce352da20b
Vadim_Sedelnikov <Burma_Olga@bk.ru>
2022-08-08 23:51:28 +0300

    change js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = bracketsConfig.join('').replace(/,/g, '');
+  let res = [];
+for (let bracket of str) {
+      let bracketsIndex = brackets.indexOf(bracket)
+
+      if (bracketsIndex % 2 === 0) {
+          
+          if (bracket === brackets[bracketsIndex + 1] && res[res.length - 1] === bracketsIndex){
+              res.pop();
+          } else if (bracket === brackets[bracketsIndex + 1] && res[res.length - 1] !== bracketsIndex) {
+              res.push(bracketsIndex)
+          }
+          else{
+              res.push(bracketsIndex)
+          }
+      } 
+      else {
+          if (res.pop() !== bracketsIndex-1){
+              return false;
+          }
+      }
+      
+  }
+  return res.length === 0
 }
